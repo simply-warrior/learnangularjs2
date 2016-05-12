a7665152600a00aa02618eb11a12c7de940dc927
Henri Wilson Tompodung <henriinchrist@gmail.com>
2016-05-12 16:48:37 +0700

    Create my-component.component.ts
    mycomponent ts

---
+++ dev/my-component.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-component',
+    template: '
+        This is my component!
+    '
+})
+export class MyComponentComponent {
+    
+}
